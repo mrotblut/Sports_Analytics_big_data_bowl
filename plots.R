bf4c9f5d64f0dc20d0abc78fb01d2e1006893788
mrotblut <123139527+mrotblut@users.noreply.github.com>
2025-12-08 18:49:38 -0600

    Changed size of points

--- plots.R
+++ plots.R
@@ -253,5 +253,83 @@ anim
 
 
 
+# full play animation
+
+
+p= ggplot()  +
+  annotate("text", 
+           x = seq(40, 70, 10),
+           y = 10,
+           color = "#bebebe",
+           family = "Chivo",
+           label = 10 * c(3,4,5,4)) +
+  annotate("text", 
+           x = seq(40, 70, 10),
+           y = 40,
+           color = "#bebebe",
+           family = "Chivo",
+           label = 10 * c(3,4,5,4),
+           angle = 180) +
+  annotate("text", 
+           x = setdiff(seq(35, 75, 1), seq(35, 75, 5)),
+           y = 0,
+           color = "#bebebe",
+           label = "—",
+           angle = 90) +
+  annotate("text", 
+           x = setdiff(seq(35, 75, 1), seq(35, 75, 5)),
+           y = 160 / 3,
+           color = "#bebebe",
+           label = "—",
+           angle = 90) +
+  annotate("text", 
+           x = setdiff(seq(35, 75, 1), seq(35, 75, 5)),
+           y = 23.36667,
+           color = "#bebebe",
+           label = "–",
+           angle = 90) +
+  annotate("text", 
+           x = setdiff(seq(35, 75, 1), seq(35, 75, 5)),
+           y = 29.96667,
+           color = "#bebebe",
+           label = "–",
+           angle = 90) +
+  geom_vline(xintercept = seq(35, 75, 5), color = "#bebebe") +
+  geom_point(data = one_play, aes(x,y,color = player_side), size = 7.5)+
+  geom_point(data = ball, aes(ball_land_x,ball_land_y),shape = "x", size = 6, color = 'brown')+
+  theme_minimal() +
+  labs(title = "<span style = 'color:#A5ACAF;'>**Las Vegas Raiders**</span> vs. <span style = 'color:#C83803;'>**Chicago Bears**</span>, 2023 NFL Week 7",
+       subtitle = sup$play_description) +
+  theme(panel.background = element_rect(fill = "white"),
+        legend.position = "none",
+        plot.subtitle = element_text(size = 9, face = "italic", hjust = 0.5),
+        plot.title = ggtext::element_markdown(hjust = 0.5, size = 12),
+        text = element_text(family = "Chivo", color = "#26282A"),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank())+
+  scale_color_manual(values = c(Defense = "#C83803", Offense = "#A5ACAF"))+
+  transition_time(frame_id)+
+  annotate("segment", 
+         x = 15,
+         xend = 65,
+         y = c(-Inf, Inf),
+         yend = c(-Inf, Inf),
+        color = "#bebebe") +
+    scale_size_identity() +
+    scale_fill_identity() +
+    ease_aes("linear") +
+    coord_cartesian(xlim = c(35, 75), ylim = c(0, 160 / 3), expand = FALSE)
+
+anim <- animate(
+  p,
+  fps = 10,
+  width = 900,
+  height = 400,
+  duration = max(filled_in$frame_id) / 10  # ensures 1 frame per actual frame_id
+)
+
+anim_save("full_play.gif", animation = anim)
 
 
